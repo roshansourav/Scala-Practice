414c2e01b91474d68743a16cb65be3ca562ac466
roshan.k <roshan.k@techsophy.com>
2022-12-02 01:37:29 +0530

    scala practice fiday 02 dec

---
+++ 02 BackSlash Character/BackSlash.scala
@@ -0,0 +1,11 @@
+object BackSlash{
+    def main(args: Array[String]){
+        println("hello \t Bro");
+        print("the Crazy Fox");
+        print("jumped\n");
+        print("all of a sudden");
+        print("\bNEW");
+        print("\n");
+        println();
+    }
+}

b0fe9dbfbcd2bcf5f31c47593989029175162448
roshan.k <roshan.k@techsophy.com>
2022-12-06 11:39:10 +0530

    Operators

---
+++ 07 Operators/Operators.scala
@@ -0,0 +1,9 @@
+object Operators{
+    def main(args : Array[String]){
+        var a : Int = 12;
+        var b : Int = 24;
+        var sum : Int = a + b;
+        println("sum = "+sum);
+
+    }
+}
